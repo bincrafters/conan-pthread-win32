be1840e9ec261a17463e85c8f9545d1ee54335a8
bincrafters-user <bincrafters@gmail.com>
2019-10-01 02:04:27 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.12.1

--- conanfile.py
+++ conanfile.py
@@ -18,15 +18,15 @@ class PthreadWin32Conan(ConanFile):
     # Options may need to change depending on the packaged library.
     settings = {"os": "Windows", "arch": None, "compiler": "Visual Studio", "build_type": None}
     options = {"shared": [True, False]}
-    default_options = "shared=False"
-    source_subfolder = "source_subfolder"
+    default_options = {'shared': 'False'}
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         tools.get("https://github.com/GerHobbelt/pthread-win32/archive/master.zip")
-        os.rename('pthread-win32-master', self.source_subfolder)
+        os.rename('pthread-win32-master', self._source_subfolder)
 
     def build(self):
-        with tools.chdir(self.source_subfolder):
+        with tools.chdir(self._source_subfolder):
             solution_name = {15: 'pthread.2015.sln',
                              14: 'pthread.2015.sln',
                              12: 'pthread.2013.sln'}.get(int(str(self.settings.compiler.version)))
@@ -35,10 +35,10 @@ class PthreadWin32Conan(ConanFile):
             msbuild.build(solution_name, targets=targets, platforms={"x86": "Win32"})
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="COPYING", src=self.source_subfolder)
-        self.copy(pattern="pthread.h", dst="include", src=self.source_subfolder)
-        self.copy(pattern="sched.h", dst="include", src=self.source_subfolder)
-        self.copy(pattern="semaphore.h", dst="include", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="COPYING", src=self._source_subfolder)
+        self.copy(pattern="pthread.h", dst="include", src=self._source_subfolder)
+        self.copy(pattern="sched.h", dst="include", src=self._source_subfolder)
+        self.copy(pattern="semaphore.h", dst="include", src=self._source_subfolder)
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", keep_path=False)
 
